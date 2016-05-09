92b81ce299dd34e3ac118063162b75bd13443c34
SreeharshaReddy <harshapriya2353@gmail.com>
2016-05-09 13:05:05 -0700

    Todos Application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,53 @@
-var todos = angular.module('todos', []);
-todos.controller('RandomController', RandomController);
-todos.factory{'Todos', function() {
-  return {
-    get: function() {
-      return {"Hello World!"};
-    }
+function TodoCtrl($scope) {
+  $scope.todos = [];
+  $scope.markAll = false;
+
+  $scope.addTodo = function() {
+      if(event.keyCode == 13 && $scope.todoText){
+          $scope.todos.push({text:$scope.todoText, done:false});
+          $scope.todoText = '';
+      }
   };
-});
-function RandomController(Todo) {
-  this.name = Todo.get();
+  $scope.isTodo = function(){
+      return $scope.todos.length > 0;
+  }
+  $scope.toggleEditMode = function(){
+      $(event.target).closest('li').toggleClass('editing');
+  };
+  $scope.editOnEnter = function(todo){
+      if(event.keyCode == 13 && todo.text){
+          $scope.toggleEditMode();
+      }
+  };
+
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  $scope.hasDone = function() {
+      return ($scope.todos.length != $scope.remaining());
+  }
+
+  $scope.itemText = function() {
+      return ($scope.todos.length - $scope.remaining() > 1) ? "items" : "item";
+  };
+
+  $scope.toggleMarkAll = function() {
+      angular.forEach($scope.todos, function(todo) {
+        todo.done =$scope.markAll;
+      });
+  };
+
+  $scope.clear = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  };
+
 }
-todos.directive('hello', function(){
-  return {
-    restrict: 'E',
-    template: '<h1> Hello World! </h1>'
-  };
-});
-todos.directives('Jumbotron', function(){
-  return{
-    template: 
-  };;
-});
